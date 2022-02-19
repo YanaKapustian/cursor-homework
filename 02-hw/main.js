0a597c82b7259be809a04f45428eecfb3b19b38e
Yana Kapustian <65828184+YanaKapustian@users.noreply.github.com>
2022-02-19 16:27:27 +0200

    Homework 2 fixed FOR loop

--- 02-hw/main.js
+++ 02-hw/main.js
@@ -1,29 +1,43 @@
 'use strict'
 
-let N;
+let numN;
 do {
-    N = Number(prompt('Введіть ціле число N'))
-} while (!Number.isInteger(N))
+    numN = Number(prompt('Введіть ціле число N'))
+} while (!Number.isInteger(numN))
 
-let M;
+let numM;
 do {
-    M = Number(prompt(`Введіть ціле число M більше за ${N}`))
-} while (!Number.isInteger(M) || N > M);
+    numM = Number(prompt(`Введіть ціле число M більше за ${numN}`))
+} while (!Number.isInteger(numM) || numN > numM);
 
-let boolean = confirm('Потрібно пропускати парні числа?')
+const boolean = confirm('Потрібно пропускати парні числа?')
 
 let sum = 0;
 
-if (boolean) {
-    for (N; N <= M; N++){
-        if(N % 2 === 1 || N % 2 === -1){ // врахувала можливість введення від'ємних чисел
-            sum += N
+// if (boolean) {
+//     for (numN; numN <= numM; numN++){
+//         if(numN % 2 === 1 || numN % 2 === -1){ // врахувала можливість введення від'ємних чисел
+//             sum += numN
+//         }
+//     }
+//     console.log(sum);
+// } else {
+//     for (numN; numN <= numM; numN++){
+//         sum += numN
+//     }
+//     console.log(sum);
+// }
+
+//Я подумала, що буде краще спочатку перевірити boolean через if, потім запускати цикл, 
+//щоб цикл кожного разу не перевіряв boolean, однак сказали, що краще щоб був один цикл. 
+
+for (numN; numN <= numM; numN++){
+    if(boolean){
+        if(numN % 2 === 1 || numN % 2 === -1){ // врахувала можливість введення від'ємних чисел
+            sum += numN
         }
+    } else {
+        sum += numN
     }
-    console.log(sum);
-} else {
-    for (N; N <= M; N++){
-        sum += N
-    }
-    console.log(sum);
 }
+console.log(sum);
