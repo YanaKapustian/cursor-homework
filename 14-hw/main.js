0e0fc709c71518f215f949a0957d5e8ed65790a1
Yana Kapustian <65828184+YanaKapustian@users.noreply.github.com>
2022-07-11 20:49:16 +0300

    Homework 14 fixed wookiee planets

--- 14-hw/main.js
+++ 14-hw/main.js
@@ -50,70 +50,51 @@ async function createCards() {
 
 btnShow.addEventListener('click', function() {
     counter = 1;
-    bool ? showPlanetsWookiee() : showPlanets()
+    showPlanets()
 })
 
 btnNext.addEventListener('click', function() {
-    bool ? showPlanetsWookiee() : showPlanets()
+    if (counter > 5) return;
+    showPlanets()
 })
 
 async function showPlanets() {
-    
     content.innerHTML = '';
     btnNext.classList.remove('hidden')
-    const request1 = await fetch(`${BASE_URL}/planets/?page=${counter}`)
-    const request2 = await fetch(`${BASE_URL}/planets/?page=${counter + 1}`)
-    const page1 = await request1.json()
-    const page2 = await request2.json()
-    const ul = document.createElement('ul');
-    content.append(ul)
-    for (let i = 0; i < page1.results.length + page2.results.length; i++) {
-        ul.insertAdjacentHTML('beforeend', `
-            <li>${i < page1.results.length ? page1.results[i].name : page2.results[i - 10].name}</li>
-        `)
+    if (!bool) {
+        const request1 = await fetch(`${BASE_URL}/planets/?page=${counter}`)
+        const request2 = await fetch(`${BASE_URL}/planets/?page=${counter + 1}`)
+        const page1 = await request1.json()
+        const page2 = await request2.json()
+        const ul = document.createElement('ul');
+        content.append(ul)
+        for (let i = 0; i < page1.results.length + page2.results.length; i++) {
+            ul.insertAdjacentHTML('beforeend', `
+                <li>${i < page1.results.length ? page1.results[i].name : page2.results[i - 10].name}</li>
+            `)
+        }
+    } else {
+        const page1 = await fetch(`${BASE_URL}/planets/?format=wookiee&page=${counter}`)
+        .then(response => response.text())
+        .then(response => JSON.parse(response.replaceAll(`whhuanan`, `"whhuanan"`)))
+        const page2 = await fetch(`${BASE_URL}/planets/?format=wookiee&page=${counter + 1}`)
+        .then(response => response.text())
+        .then(response => JSON.parse(response.replaceAll(`whhuanan`, `"whhuanan"`)))
+        let arr1 = page1.rcwochuanaoc;
+        let arrOfNames1 = arr1.map(obj => obj.whrascwo)
+        let arr2 = page2.rcwochuanaoc;
+        let arrOfNames2 = arr2.map(obj => obj.whrascwo)
+        const ul = document.createElement('ul');
+        content.append(ul)
+        for (let i = 0; i < arrOfNames1.length + arrOfNames2.length; i++) {
+            ul.insertAdjacentHTML('beforeend', `
+                <li>${i < arrOfNames1.length ? arrOfNames1[i] :  arrOfNames1[i - 10]}</li>
+            `)
+        }
     }
     await countPages();
     counter += 2;
 }
-
-getPlanetsWookiee()
-
-async function getPlanetsWookiee() {
-    for (let i = 1; i <= 60; i++) {
-        const request = await fetch(`${BASE_URL}/planets/${i}?format=wookiee`)
-        const planetWookiee = await request.json()
-        arrOfPlanet.push(planetWookiee.whrascwo)
-    }
-}
-
-async function showPlanetsWookiee() {
-    content.innerHTML = '';
-    btnNext.classList.remove('hidden')
-    const ul = document.createElement('ul');
-    content.append(ul)
-    let index;
-    switch(counter) {
-        case 1: 
-            index = 0;
-            break;
-        case 3: 
-            index = 20;
-            break;
-        case 5: 
-            index = 40;
-            break;    
-    }
-    console.log(counter);
-    let until = index + 20;
-    for (index; index < until; index++) {
-        ul.insertAdjacentHTML('beforeend', `
-            <li>${arrOfPlanet[index]}</li>
-        `)
-    }
-    await countPages();
-    counter += 2;
-}
-
 
 function countPages() {
     page.classList.remove('hidden')
@@ -133,7 +114,6 @@ function countPages() {
 btnTranslate.addEventListener('click', translateIntoWookiee)
 
 function translateIntoWookiee() {
-    wookiee_lang = '?format=wookiee'
     bool = true;
     content.innerHTML = '';
     btnTranslate.classList.add('hidden')
@@ -144,53 +124,9 @@ function translateIntoWookiee() {
 btnStopTranslate.addEventListener("click", stopTranslating);
 
 function stopTranslating() {
-  wookiee_lang = "";
   bool = false;
   content.innerHTML = "";
   btnTranslate.classList.remove("hidden");
   btnStopTranslate.classList.add("hidden");
+  page.classList.add('hidden')
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// async function getData() {
-//     translateIntoWookiee()
-//     const request = await fetch(`${BASE_URL}/people/2/${wookiee_lang}`)
-//     const ans = await request.json()
-//     console.log(ans);
-// }
-// async function getData2() {
-//     const request = await fetch(`${BASE_URL}/planets/2/`)
-//     const ans = await request.json()
-//     console.log(ans);
-// }
-
-// getData()
-// getData2()
